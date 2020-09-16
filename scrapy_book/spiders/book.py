dd9f55be8475bef7a4b2478b748ffabe9d2403d3
hoojo <hoojo@qq.com>
2020-09-16 11:20:32 +0800

    :sparkles: :bento: :recycle:  Changed scrapy book spider core class

--- scrapy_book/spiders/book.py
+++ scrapy_book/spiders/book.py
@@ -44,20 +44,86 @@ class BookSpider(scrapy.Spider):
     allowed_domains = ['bloogle.top']
     start_urls = ['http://bloogle.top/']
 
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
+    }
+
     def parse(self, response):
 
         for line in response.css('li.menu-item-object-category:not(li.menu-item-has-children)'):
-            item = ScrapyBookItem()
-
             queue = deque([])
             self.find_parent(line, queue)
-            print("folder: ", "/".join(queue))
 
-            item["folder"] = "/".join(queue)
-            item["category"] = line.css("a::text").get().replace("/", "、")
-            item["link"] = line.css("a::attr(href)").get()
+            folder = "/".join(queue)
+            category_url = line.css("a::attr(href)").get()
+            category = line.css("a::text").get().replace("/", "、")
 
-            yield item
+            if category_url is not None:
+                request = scrapy.Request(category_url, headers=self.headers, callback=self.parse_book_list)
+                request.cb_kwargs["book"] = {"folder": folder, "category": category, "category_url": category_url}
+
+                yield request
+
+            break
+
+    def parse_book_list(self, response, book):
+
+        for article in response.css("article"):
+
+            title_node = article.css("h2.entry-title a")
+            detail_url = title_node.attrib["href"]
+            title = title_node.css("::text").get()
+
+            titles = self.get_book_name_author(title)
+            print("title: ", title)
+
+            item = ScrapyBookItem()
+
+            item["folder"] = book["folder"]
+            item["category"] = book["category"]
+            item["category_url"] = book["category_url"]
+            item["name"] = titles[0].strip().replace("《", "").replace("》", "")
+            item["author"] = titles[1].strip()
+            item["detail_url"] = detail_url
+            item["image"] = article.css("a.post-thumbnail img::attr(src)").get()
+
+            if detail_url is not None:
+                request = scrapy.Request(detail_url, headers=self.headers, callback=self.parse_book_detail)
+                request.cb_kwargs["item"] = item
+
+                yield request
+
+        next_page = response.css("nav.pagination a.next::attr(href)").get()
+        if next_page is not None:
+            print("next page:", next_page)
+            request = scrapy.Request(next_page, headers=self.headers, callback=self.parse_book_list)
+            request.cb_kwargs["book"] = book
+
+            yield request
+
+    def parse_book_detail(self, response, item):
+
+        item["download_url"] = response.css("a.wp-block-button__link::attr(href)").get()
+        yield item
+
+    @staticmethod
+    def split(title, key):
+
+        titles = title.split(key + "：")
+        if len(titles) != 2:
+            titles = title.split(key + ":")
+        if len(titles) != 2:
+            titles = title.split(key + "")
+
+        return titles
+
+    def get_book_name_author(self, title):
+
+        titles = self.split(title, "作者")
+        if len(titles) != 2:
+            titles = self.split(title, "主编")
+
+        return titles
 
     def find_parent(self, selector, queue):
         node = selector.xpath("../..")
